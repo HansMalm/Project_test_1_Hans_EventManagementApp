79722c17c33adfc5279fea9beabed31f776af6df
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-05-11 09:47:58 +0200

    add ParticipantDao.java
    and ParticipantDaoImpl.java with methods.

---
+++ src/main/java/Lexicon/Hans/dao/ParticipantDao.java
@@ -0,0 +1,10 @@
+package Lexicon.Hans.dao;
+
+import Lexicon.Hans.Model.Participant;
+
+import java.util.List;
+
+public interface ParticipantDao {
+    void save(Participant participant);
+    List<Participant> findAll();
+}
---
+++ src/main/java/Lexicon/Hans/dao/ParticipantDaoImpl.java
@@ -0,0 +1,60 @@
+package Lexicon.Hans.dao;
+
+import Lexicon.Hans.Model.Participant;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParticipantDaoImpl implements ParticipantDao{
+    private final Connection connection;
+
+    public ParticipantDaoImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void save(Participant participant) {
+        String sql = "INSERT INTO participants (participant_name) VALUES (?)";
+
+        try (
+                PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ) {
+            ps.setString(1, participant.getName());
+            ps.executeUpdate();
+
+            try (ResultSet keys = ps.getGeneratedKeys()) {
+                if (keys.next()) {
+                    participant.setId(keys.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error saving participant : " + e.getMessage());
+            throw  new RuntimeException("Error saving participant", e);
+        }
+    }
+
+    @Override
+    public List<Participant> findAll() {
+
+        List<Participant> participants = new ArrayList<>();
+        String sql = "SELECT * FROM participants";
+
+        try (
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()
+        ) {
+            while(rs.next()) {
+                participants.add(new Participant(
+                        rs.getInt("id"),
+                        rs.getString("participant_name")
+                ));
+            }
+        } catch (SQLException e) {
+            System.err.println("Error fetching events : " + e.getMessage());
+            throw  new RuntimeException("Error fetching events", e);
+        }
+
+        return participants;
+    }
+}
