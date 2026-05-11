650fd0a89aae55ba86f6cf6942758b765c3af36a
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-05-11 10:05:04 +0200

    add methods
    add EventAppController.java
    and EventAppView.java

---
+++ src/main/java/Lexicon/Hans/controller/EventAppController.java
@@ -0,0 +1,83 @@
+package Lexicon.Hans.controller;
+
+import Lexicon.Hans.Model.Event;
+import Lexicon.Hans.Model.Invitation;
+import Lexicon.Hans.Model.Participant;
+import Lexicon.Hans.dao.EventDao;
+import Lexicon.Hans.dao.InvitationDao;
+import Lexicon.Hans.dao.ParticipantDao;
+import Lexicon.Hans.view.EventAppView;
+
+import java.util.List;
+
+public class EventAppController {
+
+    EventDao eventDao;
+    ParticipantDao participantDao;
+    InvitationDao invitationDao;
+    EventAppView view;
+
+    public EventAppController(EventDao eDao, ParticipantDao pDao, InvitationDao iDao, EventAppView view) {
+        this.eventDao = eDao;
+        this.participantDao = pDao;
+        this.invitationDao = iDao;
+        this.view = view;
+    }
+
+    public void run() {
+        boolean running = true;
+        System.out.println("\nWelcome to Event Manager!");
+        while (running) {
+            EventAppView.displayMainMenu();
+            String input = EventAppView.getUserInput("Choose an option: ");
+
+            switch (input) {
+                case "1":
+                    createEvent();
+                    break;
+                case "2":
+                    createParticipant();
+                    break;
+                case "3":
+                    viewEvents();
+                    break;
+                case "4":
+                    viewParticipants();
+                    break;
+                case "0":
+                    running = false;
+                    EventAppView.displayMessage("Exit Program.");
+                    break;
+                default:
+                    EventAppView.displayMessage("Invalid input!");
+            }
+        }
+    }
+
+    private void createEvent() {
+        String title = EventAppView.getUserInput("Event Title: ");
+
+        eventDao.save(new Event(title));
+    }
+
+    private void createParticipant() {
+        String name = EventAppView.getUserInput("Name: ");
+
+        participantDao.save(new Participant(name));
+    }
+
+    private void viewEvents() {
+        List<Event> events = eventDao.findAll();
+        EventAppView.displayEvents(events);
+    }
+
+    private void viewParticipants() {
+        List<Participant> participants = participantDao.findAll();
+        EventAppView.displayParticipants(participants);
+    }
+
+    private void viewInvitations() {
+        List<Invitation> invitations = invitationDao.findAll();
+        EventAppView.d
+    }
+}
---
+++ src/main/java/Lexicon/Hans/view/EventAppView.java
@@ -0,0 +1,62 @@
+package Lexicon.Hans.view;
+
+import Lexicon.Hans.Model.Event;
+import Lexicon.Hans.Model.Invitation;
+import Lexicon.Hans.Model.Participant;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class EventAppView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void displayMainMenu() {
+        System.out.print("""
+                    \nPlease select an option.
+                    1. Create a new Event.
+                    2. Register Participant.
+                    3. View Events.
+                    4. View Participants.
+                    0. Exit.
+                    """);
+    }
+
+    public static String getUserInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public static void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void displayEvents(List<Event> events) {
+        if (events.isEmpty()) {
+            System.out.println("List is empty.");
+        }
+
+        System.out.println("\nAll Events.");
+        System.out.println("ID Title");
+        events.forEach(System.out::println);
+    }
+
+    public static void displayParticipants(List<Participant> participants) {
+        if (participants.isEmpty()) {
+            System.out.println("List is empty.");
+        }
+
+        System.out.println("\nAll Participants.");
+        System.out.println("ID Name");
+        participants.forEach(System.out::println);
+    }
+
+    public static void displayInvitations(List<Invitation> invitations) {
+        if (invitations.isEmpty()) {
+            System.out.println("List is empty.");
+        }
+
+        System.out.println("\nAll Invitations.");
+    }
+
+}
